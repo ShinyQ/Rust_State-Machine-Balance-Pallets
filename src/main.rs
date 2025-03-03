5ecd537612db69edc9b7abc2d489f2b655d3f42d
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2025-03-04 01:24:44 +0700

    feat: using runtime on main and define derive debug

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,43 @@
 mod balances;
 mod system;
 
+#[derive(Debug)]
+pub struct Runtime {
+	system: system::Pallet,
+	balances: balances::Pallet,
+}
+
+impl Runtime {
+	fn new() -> Self {
+		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
+	}
+}
+
 fn main() {
-	println!("Hello, world!");
+	let mut runtime = Runtime::new();
+	let kurniadi = "kurniadi".to_string();
+	let ahmad = "ahmad".to_string();
+	let wijaya = "wijaya".to_string();
+
+	runtime.balances.set_balance(&kurniadi, 200000);
+	runtime.system.inc_block_number();
+	assert_eq!(runtime.system.block_number(), 1);
+
+	runtime.system.inc_nonce(&kurniadi);
+
+	let _res = runtime
+		.balances
+		.transfer(kurniadi.clone(), ahmad, 10000)
+		.map_err(|e| eprintln!("{}", e));
+
+	runtime.system.inc_nonce(&kurniadi);
+	let _res = runtime
+		.balances
+		.transfer(kurniadi, wijaya, 50000)
+		.map_err(|e| eprintln!("{}", e));
+
+	let current_balance = runtime.balances.balance(&"kurniadi".to_string());
+	
+	println!("Kurniadi current balances: {}", current_balance);
+	println!("{:#?}", runtime);
 }
